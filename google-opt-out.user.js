d6a3888ae2de14b37a8a0869d7f9a91cfad1454f
Neil-Smithline <github.com@neilsmithline.com>
2012-07-15 12:12:04 -0400

    added google-opt-out.user.js

---
+++ google-opt-out.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             google-opt-out@neilsmithline.com
+// @name           Google: Opt-Out
+// @version        0.0.1
+// @namespace      com.neilsmithline
+// @author         Neil Smithline http://www.neilsmithline.com
+// @developer      Neil Smithline http://www.neilsmithline.com
+// @description    Opt-out, as much as possible, from Google Analitics
+// @updateURL      https://raw.github.com/Neil-Smithline/UserScripts/master/timely-google-search-year.user.js
+// @supportURL     https://github.com/Neil-Smithline/UserScripts/issues
+// @include        *
+// @run-at         document-start
+// ==/UserScript==
+
+_ga_user_prefs = false;
